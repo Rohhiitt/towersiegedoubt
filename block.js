e69d7016fb432891769d22bdadfb6fd7224cd9a8
Rohhiitt <75480492+Rohhiitt@users.noreply.github.com>
2021-05-16 19:48:32 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,24 @@
+class Block{
+    constructor(x, y, width, height) {
+        var options = {
+                     
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+        this.image = loadImage("block.png");
+      }
+      display(){
+        var angle = this.body.angle;
+        var pos= this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        imageMode(CENTER)
+        rectMode(CENTER);
+        rect(0,0,this.width, this.height);
+        image(this.image,block.position.x,block.position.y,40,40);
+        pop();
+      }
+}
